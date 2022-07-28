162ddc9cb02934a381243ad7a05178c7022c9dc2
Yash Sharma <44340561+yash-sharma1@users.noreply.github.com>
2022-07-28 13:48:55 +0530

    Name & description changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'demo',
-  tagline: 'Dinosaurs are cool',
+  title: 'Deployment Demo',
+  tagline: 'Testing Docusaurus & GitHub Pages',
   url: 'https://github.com',
   baseUrl: '/demo/',
   onBrokenLinks: 'throw',
