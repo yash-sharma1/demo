79b61daf987ce411c75eeef71ecb49e5c5e0aa89
Yash Sharma <44340561+yash-sharma1@users.noreply.github.com>
2022-08-22 02:48:51 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Deployment Demo',
   tagline: 'Testing Docusaurus & GitHub Pages',
   url: 'https://github.com',
-  baseUrl: '/demo/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
