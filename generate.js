b95152d4a9ffc3d64dc012dc3c157277f4965a6b
Yash Sharma <44340561+yash-sharma1@users.noreply.github.com>
2022-08-22 15:40:16 +0530

    Update generate.js

--- generate.js
+++ generate.js
@@ -1,2 +1,2 @@
 const fs = require('fs')
-fs.writeFileSync('docs/generated-markdown.mdx', '# Generation Test', 'utf-8')
+fs.writeFileSync('docs/generated-markdown.mdx', '# Generation Test #2', 'utf-8')
